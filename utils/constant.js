ce35279ce2ad46972726a4fb36d93fa12815b81c
sudhanshukuumar <116362103+sudhanshukuumar@users.noreply.github.com>
2024-09-07 15:39:11 +0530

    Update constant.js

--- utils/constant.js
+++ utils/constant.js
@@ -3,7 +3,7 @@
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-export const USER_API_END_POINT="https://talentify-b.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://talentify-b.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://talentify-b.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://talentify-b.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://talentify-backend-h1ad.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://talentify-backend-h1ad.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://talentify-backend-h1ad.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://talentify-backend-h1ad.onrender.com/api/v1/company";
